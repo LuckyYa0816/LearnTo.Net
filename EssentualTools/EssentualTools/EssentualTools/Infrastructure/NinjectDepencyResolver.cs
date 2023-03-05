7f41fa5b195bb218db3ca7af4149aad4faf4fb28
Excuses <1624605666@qq.com>
2023-03-05 22:19:34 +0800

    使用Ninject

---
+++ EssentualTools/EssentualTools/EssentualTools/Infrastructure/NinjectDepencyResolver.cs
@@ -0,0 +1,43 @@
+﻿using EssentualTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentualTools.Infrastructure
+{
+    public class NinjectDepencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDepencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind <IValueCalculator>().To<LinqValueCalculator>().InSingletonScope();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>()
+                //.WithPropertyValue("DiscountSize",50M);
+                .WithConstructorArgument("discountParam",50M);
+            kernel.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
+                .WhenInjectedInto<LinqValueCalculator>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+
+    }
+}
---
+++ EssentualTools/EssentualTools/EssentualTools/Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentualTools.Models
+{
+    public class ShoppingCart
+    {
+        //private LinqValueCalculator calc;
+        private IValueCalculator calc;
+
+        public ShoppingCart(IValueCalculator calcParam)
+        {
+            //this.calc = calcParam;
+            calc= calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalcuateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
