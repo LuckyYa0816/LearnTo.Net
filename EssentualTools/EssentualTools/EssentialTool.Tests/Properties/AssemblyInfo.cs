c313c1cb80b3772e1661a9a93ab1758de41ba9d1
Excuses <1624605666@qq.com>
2023-03-06 22:38:08 +0800

    单元测试和Moq工具

---
+++ EssentualTools/EssentualTools/EssentialTool.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTool.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssentialTool.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af7a71ba-db55-4cc8-9dfc-019c7e580f1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssentualTools/EssentualTools/EssentialTool.Tests/UnitTest2.cs
@@ -0,0 +1,41 @@
+﻿using EssentualTools.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+
+namespace EssentialTool.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+            new Product{Name = "Kayak",Category="Watersports",Price=275M},
+            new Product{Name = "Lifejacket",Category="Watersports",Price=48.95M},
+            new Product{Name = "Soccer ball",Category="Soccer",Price=19.50M},
+            new Product{Name = "Corner flag",Category="Soccer",Price=34.95M}
+        };
+
+        [TestMethod]
+        public void Sum_Products_Correctly()
+        {
+            /*
+            //准备
+            var discounter = new MiniumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = products.Sum(e => e.Price);
+            */
+
+            //准备
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
+
+            //动作
+            var result = target.ValueProducts(products);
+
+            //断言
+            Assert.AreEqual(products.Sum(e=>e.Price),result);
+        }
+    }
+}
